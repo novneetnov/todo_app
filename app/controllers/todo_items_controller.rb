aa3aa62a77a010f0e3c2f1cd79eb7f56ab7a770e
novneetnov <novneet346iith@gmail.com>
2015-12-09 18:58:21 +0530

    Mark as Complete Ability

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
 	before_action :set_todo_list
+	before_action :set_todo_item, except: [:create]
 
 	def create
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -7,13 +8,22 @@ class TodoItemsController < ApplicationController
 	end
 
 	def destroy
-		@todo_item = @todo_list.todo_items.find(params[:id]).destroy
+		@todo_item.destroy
 		redirect_to @todo_list, notice: "Todo item successfully deleted"		
+	end
+
+	def complete
+		@todo_item.update_attribute(:completed_at, Time.now)
+		redirect_to @todo_list, notice: "Todo Item Completed"
 	end
 
 	private
 	def set_todo_list
 		@todo_list = TodoList.find(params[:todo_list_id])
+	end
+	
+	def set_todo_item
+		@todo_item = @todo_list.todo_items.find(params[:id])
 	end
 
 	def todo_item_params
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,7 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
+
+	def completed?
+		!self.completed_at.blank?
+	end
 end
