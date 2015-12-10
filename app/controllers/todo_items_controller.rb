fb38555b0864d19c03afbd2d78ace658eac30a4a
novneetnov <novneet346iith@gmail.com>
2015-12-10 22:06:55 +0530

    Finish Structure & Styles

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -4,7 +4,7 @@ class TodoItemsController < ApplicationController
 
 	def create
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
-		redirect_to @todo_list
+		redirect_to @todo_list, notice: "Todo Item Created"
 	end
 
 	def destroy
